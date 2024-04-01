0f53a012970e2057557a2778322ebf57a6ba0289
sxiao135 <133243907+sxiao135@users.noreply.github.com>
2024-04-01 14:42:27 -0500

    start chosing colors function

---
+++ Assets/Susan/ColorPicker.cs
@@ -0,0 +1,15 @@
+//change the color based on the input size
+//go around the color wheel
+// rgb: preferably btwn 100-255
+//The hex code circle works by increasing r, reaching 255, increasing g, reaching 255, 
+// decreasing r to 0, then increasing b to 225, an then finally decreasing g to 0
+// try instead of increasing/decreasing around 0, have a different middle (like 14 or 153 or sth)
+
+public class ColorPicker {
+    public void choose(){ //hoping for input as the # in line of nurse
+        //this for loop is temporary
+        for (int i = 0; i < 10; i++){ //if there was only 10 objects
+            
+        }
+    }
+}
